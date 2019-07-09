140fc458bc8cf375beb16cb2ea0ed233c5758486
ehsan <ehsan.set1@gmail.com>
2019-07-09 11:17:20 -0700

    feature sidebar desine for big screens finished

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch,faPlus } from '@fortawesome/free-solid-svg-icons'; 
+import { faHome,faPlus,faGlobe,faPenNib,faTh,faBookmark } from '@fortawesome/free-solid-svg-icons'; 
 import { NavLink } from "react-router-dom";
 
 const SidebarComponent = () => {
@@ -14,19 +14,33 @@ const SidebarComponent = () => {
         <div className="sidebar h-100 px-0">
             <ul className="noBullet text-light">
                 <li>
-                    <NavLink className="text-light"  exact to={`/snippet/-1`} >
-                        <FontAwesomeIcon icon={faPlus} /><br/>
-                        New
+                    <NavLink className="sidebarMenuItem d-flex flex-column"  exact to={`/snippet/-1`} >
+                        <FontAwesomeIcon className="NavIcon" icon={faHome} />
+                        <span className="fontSize07 " >Home</span>
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink className=""  exact to={`/snippet/-1`} >
-                        <FontAwesomeIcon icon={faPlus} />
+                    <NavLink className="sidebarMenuItem d-flex flex-column"  exact to={`/snippet/-1`} >
+                        <FontAwesomeIcon className="NavIcon" icon={faPenNib } />
+                        <span className="fontSize07 " >Add</span>
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink className=""  exact to={`/snippet/-1`} >
-                        <FontAwesomeIcon icon={faPlus} />
+                    <NavLink className="sidebarMenuItem d-flex flex-column justify-content-center "  exact to={`/snippet/-1`} >
+                        <FontAwesomeIcon className="NavIcon" icon={faBookmark} />
+                        <span className="fontSize07 " >My Snippets</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink className="sidebarMenuItem d-flex flex-column justify-content-center"  exact to={`/snippet/-1`} >
+                        <FontAwesomeIcon className="NavIcon" icon={faGlobe} />
+                        <span className="fontSize07 " >Discover</span>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink className="sidebarMenuItem d-flex flex-column"  exact to={`/snippet/-1`} >
+                        <FontAwesomeIcon className="NavIcon" icon={faTh} />
+                        <span className="fontSize07" >Collection</span>
                     </NavLink>
                 </li>
             </ul>
