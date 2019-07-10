9ba95b654ec3daa8ad17b5389bddf1209c63c18f
ehsan <ehsan.set1@gmail.com>
2019-07-09 18:06:55 -0700

    feature: bookmark button and share button added to the interface

--- src/components/SnippetCard.js
+++ src/components/SnippetCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
-import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark,faShareAlt } from '@fortawesome/free-solid-svg-icons'; 
 
 
 const SnippetCard = ({snippet}) => {
@@ -14,7 +14,7 @@ const SnippetCard = ({snippet}) => {
     });
 
     return (
-        <Card className="snippetCard col-12 ml-2 my-1" style={{ width: '90%' }}>
+        <div className="card snippetCard mmmm col-12 ml-2 my-3 " style={{ width: '90%' }}>
             <NavLink snippet={snippet} exact to={
                 {
                     pathname: "/snippet/" + snippet.id,
@@ -22,20 +22,26 @@ const SnippetCard = ({snippet}) => {
                 }
                 }>
                 <div className="row ">
-                    <div className="col-md-1 bg-secondary">
-                    </div>
-                    <div className="col-md-11">
-                        <Card.Body className="padding05" >
-                            <Card.Title className="d-flex justify-content-start">{snippet.title}</Card.Title>
-                            <Card.Text className="d-flex justify-content-start">
+                    <div className="col-md-12 ">
+                        <div className="card-body padding05" >
+                            <div className="card-title d-flex justify-content-start">{snippet.title}</div>
+                            <hr/>
+                            <div className="d-fle">
+                            <div className="d-flex justify-content-start m-0">
                                 {keywordsJsx}
 
-                            </Card.Text>
-                        </Card.Body>
+                            </div>
+                                <div className="d-flex justify-content-end m-0">
+                                    <FontAwesomeIcon className="fontSize12 mx-2" icon={faBookmark} />
+                                    <FontAwesomeIcon className="fontSize12 mx-2" icon={faShareAlt} />
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </NavLink>
-        </Card>
+        </div>
     )
 }
 
